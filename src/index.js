8617647db9ffcebde2e2f7cd2aff606cc5099d6d
Sergei Shipilov <sergeishipilou@yandex.by>
2020-10-05 22:23:34 +0300

    feat: add task solution morse-decoder

--- src/index.js
+++ src/index.js
@@ -1,44 +1,78 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
 };
 
 function decode(expr) {
-    // write your solution here
+  
+  let WORD_STEP = 10;
+  let MORSE_STEP = 2;
+
+  let CIPHER_MORSE = {
+    '10':  '.',
+    '11':  '-',
+    '**':  ' ',
+    '00':   ''
+  };
+
+  let parse = function(expr, step) {
+
+    let result = [];
+    let index = 0;
+
+    while (index < expr.length) {
+      result.push(expr.slice(index, index += step));
+    }
+    return result;
+  }
+
+  let bitCode = parse(expr, WORD_STEP)
+    .map(function(item) {
+      return parse(item, MORSE_STEP)
+        .map(function(item) {
+          return CIPHER_MORSE[item];
+        }).join('');
+    });
+
+  return bitCode.map(function(item) {
+    if (item === '     ') return ' ';
+    else return MORSE_TABLE[item];
+  }).join('');
+
 }
 
 module.exports = {
